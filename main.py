c9582f42e1f01eda4d541e605e402b96d2082440
Isha Kashif <ishak@udel.edu>
2025-08-11 10:37:55 -0400

    basic layout

---
+++ main.py
@@ -0,0 +1,17 @@
+# Python Banking Program
+
+def show_balance():
+    pass
+
+def deposit():
+    pass
+
+def withdraw():
+    pass
+
+balance = 0
+is_running = True
+
+while is_running == True:
+    pass
+
