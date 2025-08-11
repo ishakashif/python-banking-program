2df87b508772605663eac23bbd15102c508f5dce
Isha Kashif <ishak@udel.edu>
2025-08-11 10:58:56 -0400

    added decorative text

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 # Python Banking Program
 
 def show_balance(balance):
+    print("*********************")
     print(f"Your balance is ${balance:.2f}")
-
+    print("*********************")
 def deposit():
+    print("*********************")
     amount = float(input("Enter an amount to be deposited: "))
+    print("*********************")
     if amount < 0:
+        print("*********************")
         print("Invalid Amount")
+        print("*********************")
         return 0
     else:
         return amount
 
 def withdraw(balance):
+    print("*********************")
     amount = float(input("Enter the amount to be withdrawn: "))
+    print("*********************")
     if amount > balance:
+        print("*********************")
         print("Insufficient Funds")
+        print("*********************")
         return 0
     elif amount < 0:
+        print("*********************")
         print("Amount must be greater than 0")
+        print("*********************")
         return 0
     else:
         return amount
@@ -27,12 +38,14 @@ def main():
     is_running = True
 
     while is_running == True:
-        print("Banking Program!")
+        print("*********************")
+        print("   Banking Program!   ")
+        print("*********************")
         print("1. Show Balance")
         print("2. Deposit")
         print("3. Withdraw")
         print("4. Exit")
-
+        print("**********************")
         choice = input("Enter your choice (1-4): ")
         if choice == '1':
             show_balance(balance)
@@ -45,9 +58,13 @@ def main():
         elif choice == "4":
             is_running = False
         else:
+            print("*********************")
             print("That is not a valid choice")
+            print("*********************")
 
+    print("*********************")
     print("Thank you! Have a nice day.")
+    print("*********************")
 
 if __name__ == '__main__':
     main()
