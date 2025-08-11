1865f7cdf2bdfb2d65f6663ba484d08bf4bacbdf
Isha Kashif <ishak@udel.edu>
2025-08-11 10:44:29 -0400

    finished show_balance function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Python Banking Program
 
 def show_balance():
-    pass
+    print(f"Your balance is ${balance:.2f}")
 
 def deposit():
-    pass
+    amount = input("Enter an amount to be deposited: ")
 
 def withdraw():
     pass
@@ -13,5 +13,23 @@ balance = 0
 is_running = True
 
 while is_running == True:
-    pass
+    print("Banking Program!")
+    print("1. Show Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = input("Enter your choice (1-4): ")
+    if choice == '1':
+        show_balance()
+    elif choice == "2":
+        deposit()
+    elif choice == "3":
+        withdraw()
+    elif choice == "4":
+        is_running = False
+    else:
+        print("That is not a valid choice")
+
+print("Thank you! Have a nice day.")
 
